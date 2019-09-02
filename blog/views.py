d03ddaab6c779d6b9e5aa657ed4279a5d62b4be6
Yosh <josuearguetaorozco20@gmail.com>
2019-09-02 12:20:20 -0600

    configuramos vistas

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.utils import timezone
 # Create your views here.
+from .models import Publicacion
+
+def listar_pub(request):
+    pubs = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+    return render(request, 'blog/listar_pub.html', {'pubs':pubs})
